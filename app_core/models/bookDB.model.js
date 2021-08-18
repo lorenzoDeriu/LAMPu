b0401e67aa1e65f6b3360c43c8863a727b533442
Lorenzo Deriu <deriu.lorenzo.00@gmail.com>
2021-08-18 17:36:36 +0200

    Aggiunge mongoose schema BookDBmodel

--- app_core/models/bookDB.model.js
+++ app_core/models/bookDB.model.js
@@ -1,12 +1,39 @@
-const Book = require("book.models.js");
- 
-module.exports = {
-	BookDB: class {
-		constructor(book) {
-			this.title = book.title;
-			this.authors = book.authors;
-			this.isbn = book.isbn;
-			this.thumbnail = book.thumbnail;
-		}
+const BookModel = require("book.models.js");
+const mongoose = require('mongoose'); 
+
+class BookDB_model {
+	constructor(book) {
+		this.title = book.title;
+		this.authors = book.authors;
+		this.isbn = book.isbn;
+		this.thumbnail = book.thumbnail;
 	}
 }
+
+module.exports = BookDB;
+
+bookDB_schema: mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	authors: {
+		type: [String],
+		required: true
+	},
+	isbn: {
+		type: [String],
+		required: true
+	},
+	thumbnail: {
+		type: [String],
+		required: false
+	},
+	publishDate: {
+		type: String,
+		required: false
+	}
+});
+
+const Book = new mongoose.model('Book', bookDB_schema);
+module.exports = Book;
