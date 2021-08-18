916588a98cb7147621738045b0c283545141d5be
Lorenzo Deriu <deriu.lorenzo.00@gmail.com>
2021-08-18 15:56:47 +0200

    Aggiunge il model bookDB

---
+++ app_core/models/bookDB.model.js
@@ -0,0 +1,12 @@
+const Book = require("book.models.js");
+
+module.exports = {
+	BookDB: class {
+		constructor(book) {
+			this.title = book.title;
+			this.authors = book.authors;
+			this.isbn = book.isbn;
+			this.thumbnail = book.thumbnail;
+		}
+	}
+}
